97d607bfbf1f5ccf5de33f79e115c128300e3865
Taro Omiya <taro.omiya@omiyagames.com>
2015-01-25 12:49:26 -0500

    Adding rule shuffler.

---
+++ Assets/Scripts/RuleShuffler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuleShuffler : MonoBehaviour
+{
+    [SerializeField]
+    GameObject starterRules;
+    [SerializeField]
+    GameObject restOfTheRules;
+
+    Rule[] allStarters;
+    Rule[] allRules;
+
+	// Use this for initialization
+	void Awake()
+    {
+        allStarters = starterRules.GetComponentsInChildren<Rule>();
+        allRules = restOfTheRules.GetComponentsInChildren<Rule>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
