7f6af70307233fcb41843c27a3dd47610045aa7e
Taro Omiya <taro.omiya@omiyagames.com>
2015-01-25 12:20:35 -0500

    Working on printing text for the rules panels.

---
+++ Assets/Scripts/Rule.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rule : MonoBehaviour
+{
+    public enum Suit
+    {
+        Spades,
+        Clovers,
+        Hearts,
+        Diamonds,
+        NumSuits
+    }
+    [TextArea]
+    [SerializeField]
+    string description = "Rules text";
+    [Header("Random Number")]
+    [SerializeField]
+    int min = 1;
+    [SerializeField]
+    int max= 13;
+
+    public void AppendRule(System.Text.StringBuilder builder)
+    {
+        builder.AppendFormat(description, Random.Range(min, (max + 1)), (Suit)Random.Range(0, (int)Suit.NumSuits));
+    }
+}
